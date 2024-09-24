f4ce2c54f4537a662b8c378b3e9de792a903f44f
akib1437 <akibtanzim8@gmail.com>
2024-09-24 19:11:58 +0600

    features trans done

---
+++ js/features.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-show-don').addEventListener('click', function(){
+    showSectionById('don-form');
+})
+
+document.getElementById('btn-show-his').addEventListener('click', function(){
+    showSectionById('trans-sec');
+})
